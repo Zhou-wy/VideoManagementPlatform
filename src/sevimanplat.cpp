a9b12bc56996d7a815573f476d59c544967e5834
zwyywz <2867283770@qq.com>
2023-03-04 16:28:26 +0800

    新增注释

--- src/sevimanplat.cpp
+++ src/sevimanplat.cpp
@@ -109,12 +109,18 @@ void SeViManPlat::onTimerOut() {
     ui->cpuLabel->setText(QString("   CPU ") + JQCPUMonitor::cpuUsagePercentageDisplayString());
 }
 
+/*!
+ * @brief: 初始化摄像头布局
+ * */
 void SeViManPlat::initCameraLayout() {
     videoWidget = new VideoPanel(ui->videoPanwidget);
     videoWidgetLayout = new QVBoxLayout(ui->videoPanwidget);
     videoWidgetLayout->addWidget(videoWidget);
 }
 
+/*!
+ * @brief: 导入样式表
+ * */
 void SeViManPlat::loadStyle(const QString &qssFile) {
     //加载样式表
     QString qss;
@@ -139,6 +145,9 @@ void SeViManPlat::loadStyle(const QString &qssFile) {
     }
 }
 
+/*!
+ * @brief: 读取摄像头配置JSON，程序开始初始化
+ * */
 void SeViManPlat::Json2VideoConf() {
     VideoConfJson = new Json(m_video_conf_json, true);
     if (VideoConfJson->getString("VideoCount") == "") {
@@ -155,12 +164,6 @@ void SeViManPlat::Json2VideoConf() {
             QString video_name = "video" + QString::number(i + 1);
             iINFO(ROOT_LOG) << "video_name: " << video_name.toStdString();
             std::shared_ptr<VideoConf> videoConf_i = nullptr;
-//            videoConf_i->video_name = video_name;
-
-//            // 右侧设备树创建子节点
-//            auto *webCamItem = new QTreeWidgetItem();
-//            webCamItem->setText(0, "摄像头" + QString::number(i + 1));
-//            webCamRoot->addChild(webCamItem);
 
             if (video_type.toLower() == "rtsp") {
                 QString main_code_stream = "rtsp://" + VideoConfJson->getString(video_name + ".username") + ":" +
@@ -178,15 +181,6 @@ void SeViManPlat::Json2VideoConf() {
 
                 videoConf_i = std::make_shared<VideoConf>(
                         VideoConf{video_name, {main_code_stream, Auxiliary_code_stream}});
-//                // 创建主码流和辅码流
-//                auto *webCamMain_code_stream = new QTreeWidgetItem();
-//                webCamMain_code_stream->setText(0, "主码流");
-//                webCamItem->addChild(webCamMain_code_stream);
-//                auto *webCamAuxiliary_code_stream = new QTreeWidgetItem();
-//                webCamAuxiliary_code_stream->setText(0, "辅码流");
-//                webCamItem->addChild(webCamAuxiliary_code_stream);
-//                iDEBUG(ROOT_LOG) << video_name.toStdString() << " : " << main_code_stream.toStdString();
-
             } else if (video_type.toLower() == "rtmp") {
                 url = VideoConfJson->getString(video_name + ".url");
                 if (url == "") {
@@ -218,23 +212,25 @@ void SeViManPlat::Json2VideoConf() {
                 iERROR(ROOT_LOG) << "This video" << i << " type is " << video_type.toStdString()
                                  << ", which does not belong to the preset video type!"
                                  << video_type.toStdString();
-            if (!m_videoConf.contains(videoConf_i)) // 判断videoConf 是否在，如果存在就不要新增了
-                m_videoConf.append(videoConf_i);
+            if (!this->m_videoConf.contains(videoConf_i)) // 判断videoConf 是否在，如果存在就不要新增了
+                this->m_videoConf.append(videoConf_i);
         }
     }
 }
 
+/*!
+ * @brief: m_videoConf格式化Json写入配置文件，程序退出时写入
+ */
 void SeViManPlat::VideoConf2Json() {
-
     //将m_videoConf按video_name由小到大排序
-    std::sort(m_videoConf.begin(), m_videoConf.end(),
+    std::sort(this->m_videoConf.begin(), this->m_videoConf.end(),
               [](const std::shared_ptr<VideoConf> &video1, const std::shared_ptr<VideoConf> &video2) {
                   return video1->video_name < video2->video_name;
               });
     // 摄像头个数
-    VideoConfJson->set("VideoCount", QString::number(m_videoConf.length()));
+    VideoConfJson->set("VideoCount", QString::number(this->m_videoConf.length()));
     //遍历m_videoConf，将配置写入JSON
-    for (const auto &conf: m_videoConf) {
+    for (const auto &conf: this->m_videoConf) {
         if (conf->urls.length() == 2) { // RTSP
             /*"rtsp://admin:admin123@192.168.0.213:554/cam/realmonitor?channel=1&subtype=0";*/
             // 提取_type
@@ -284,13 +280,16 @@ void SeViManPlat::VideoConf2Json() {
     VideoConfJson->save(m_video_conf_json);
 }
 
+/*!
+ * @brief: 初始化视频播放
+ */
 void SeViManPlat::initVideoPlay() {
     if (VideoConfJson->getString("VideoCount") == "") {
         iERROR(ROOT_LOG) << "The videoListConf file is empty";
     } else {
         QString url = "";
         int i = 0;
-        for (const auto &conf: m_videoConf) { // 遍历所有的摄像头配置
+        for (const auto &conf:this->m_videoConf) { // 遍历所有的摄像头配置
             QString video_type = conf->urls[0].split("://").at(0);
             // 设置右侧设备树
             QTreeWidgetItem *webCamRoot = ui->camDeviceList->topLevelItem(0);
@@ -375,14 +374,20 @@ void SeViManPlat::destroyAll() {
     qDeleteAll(videoPlayLayout);
 }
 
+/*!
+ * @brief: 新增摄像头弹窗
+ */
 void SeViManPlat::addVideoDialog() {
     this->addVideoTool = new addVideo(this);
     this->addVideoTool->show();
     connect(this->addVideoTool, SIGNAL(finished(int)), this, SLOT(addVideoPlay()));
 }
 
+/*!
+ * @brief: 新增摄像头播放
+ */
 void SeViManPlat::addVideoPlay() {
-    this->addVideoTool->saveVideoListConf(m_videoConf);
+    this->addVideoTool->saveVideoListConf(this->m_videoConf);
 
     int videoCount = this->m_videoConf.length();
     iINFO(ROOT_LOG) << "video Count : " << videoCount;
@@ -441,7 +446,7 @@ void SeViManPlat::onTreeViewClickedStream(const QModelIndex &index) {
         int video_i = parentData.toString().split("摄像头-video").at(1).toInt();
         QString url = "";
         QString findVideoName = "video" + QString::number(video_i);
-        iDEBUG(ROOT_LOG) << "clicked the video: " << video_i << " len m_videoConf: " << m_videoConf.length();
+        iDEBUG(ROOT_LOG) << "clicked the video: " << video_i << " len m_videoConf: " << this->m_videoConf.length();
 
         for (auto item: this->m_videoConf) {
             if (item->video_name == findVideoName)
@@ -460,6 +465,12 @@ void SeViManPlat::onTreeViewClickedStream(const QModelIndex &index) {
     }
 }
 
+/*
+ * @brief 删除摄像头
+ *         1、删除json文件中的关于选中摄像头的信息
+ *         2、删除m_videoConf在关于摄像头中的信息
+ *         3、删除QTreeWidget *camDeviceList;中关于摄像头的信息
+ * */
 void SeViManPlat::delVideoPlay() {
     QMessageBox::StandardButton reply = QMessageBox::question(this, "安防视频监控管理平台", "确认删除摄像头吗？",
                                                               QMessageBox::Yes | QMessageBox::No);
@@ -475,12 +486,6 @@ void SeViManPlat::delVideoPlay() {
 
 }
 
-/*
- * @brief 删除摄像头
- *         1、删除json文件中的关于选中摄像头的信息
- *         2、删除m_videoConf在关于摄像头中的信息
- *         3、删除QTreeWidget *camDeviceList;中关于摄像头的信息
- * */
 void SeViManPlat::onTreeViewClickedDelVideo(const QModelIndex &index) {
     // 获取点击的节点的行号、数据和父节点索引
     int row = index.row();
