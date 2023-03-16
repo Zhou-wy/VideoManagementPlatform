27990612899feaf3a0cf6f96073357eecc7de8e7
zwyywz <2867283770@qq.com>
2023-03-16 09:56:36 +0800

    msvc

--- cmake/3drparty.cmake
+++ cmake/3drparty.cmake
@@ -4,11 +4,11 @@ if (CMAKE_SYSTEM_NAME MATCHES "Linux")
     set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/0build-linux)
 
 elseif (CMAKE_SYSTEM_NAME MATCHES "Windows")
-    set(CMAKE_PREFIX_PATH "D:/Qt5.12.5/5.12.5/mingw73_64")
+    set(CMAKE_PREFIX_PATH "D:/Qt5.12.5/5.12.5/msvc2017_64")
     set(FFMPEG_DIR ${PROJECT_SOURCE_DIR}/3rdparty/ffmpeg4)
+    set(QUCSDK_DIR ${PROJECT_SOURCE_DIR}/3rdparty/qucsdk)
     set(SPDLOG_DIR ${PROJECT_SOURCE_DIR}/3rdparty/spdlog)
 
-    set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/0build-windows)
 
 elseif (CMAKE_SYSTEM_NAME MATCHES "Darwin")
     set(CMAKE_PREFIX_PATH "/opt/homebrew/Cellar/qt@5/5.15.8_1/lib/cmake/Qt5")
@@ -28,6 +28,9 @@ set(Qt5_Libs Qt5::Core
 
 include_directories(${FFMPEG_DIR}/include) # FFMPEG头文件搜索路径
 include_directories(${SPDLOG_DIR}/include) # gLog头文件搜索路径
+include_directories(${QUCSDK_DIR}/include) # qucsdk头文件搜索路径
+
 
 link_directories(${FFMPEG_DIR}/lib) # FFMPEG动态链接库或静态链接库的搜索路径
 link_directories(${SPDLOG_DIR}/lib) # gLog动态链接库或静态链接库的搜索路径
+link_directories(${QUCSDK_DIR}/lib) # qucsdk动态链接库或静态链接库的搜索路径
